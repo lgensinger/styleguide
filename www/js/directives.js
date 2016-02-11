a2db03e170e8a1b71e3b09fcb858dc721017c8bc
Lindsay Gensinger <lindsay@iMac.local>
2016-02-10 20:45:08 -0800

    added git submodules

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,5 +1,15 @@
 var app = angular.module("app.directives", [
+    // DO NOT EDIT BETWEEN START/END
+    // start control directives
+    "tree-list-directive",
+    "resize-buttons-directive",
+    "radio-buttons-directive",
     "radial-tree-directive",
+    "packed-circles-directive",
+    "module-title-directive",
+    "heatmap-grid-directive",
+    "drag-drop-list-directive",
+    // end control directives
     "content-slider-directive",
     "dendrogram-directive",
 	"db-item-directive",
